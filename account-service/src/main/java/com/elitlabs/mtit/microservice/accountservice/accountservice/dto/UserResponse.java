f8a5897d1cdac29cde6eb5923e6e0fff0133acab
dilshan97 <dilshanramesh81@gmail.com>
2020-04-10 22:16:35 +0530

    UserResponse created

---
+++ account-service/src/main/java/com/elitlabs/mtit/microservice/accountservice/accountservice/dto/UserResponse.java
@@ -0,0 +1,25 @@
+package com.elitlabs.mtit.microservice.accountservice.accountservice.dto;
+
+public class UserResponse {
+
+    private String userId;
+    private String message;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+}
