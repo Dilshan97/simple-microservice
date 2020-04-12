71ed42c812fd96ee7bda7ecc62e368b92ae01922
dilshan97 <dilshanramesh81@gmail.com>
2020-04-12 22:46:09 +0530

    auth initilzed

--- account-service/src/main/java/com/elitlabs/mtit/microservice/accountservice/accountservice/dto/UserRequest.java
+++ account-service/src/main/java/com/elitlabs/mtit/microservice/accountservice/accountservice/dto/UserRequest.java
@@ -1,26 +1,44 @@
 package com.elitlabs.mtit.microservice.accountservice.accountservice.dto;
 
+/**
+ *
+ */
 public class UserRequest {
 
     private String fullName;
     private String age;
 
+    /**
+     * @return
+     */
     public String getFullName() {
         return fullName;
     }
 
+    /**
+     * @param fullName
+     */
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
+    /**
+     * @return
+     */
     public String getAge() {
         return age;
     }
 
+    /**
+     * @param age
+     */
     public void setAge(String age) {
         this.age = age;
     }
 
+    /**
+     * @return
+     */
     @Override
     public String toString() {
         return "UserRequest{" +
